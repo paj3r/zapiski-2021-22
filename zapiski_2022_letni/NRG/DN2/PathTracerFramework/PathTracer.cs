b113dd3d8ba046b17ed98e2b54c21b337dc34a84
paj3r <ivo.pajer@gmail.com>
2022-04-27 14:02:30 +0200

    nrg  dn2 init

---
+++ zapiski_2022_letni/NRG/DN2/PathTracerFramework/PathTracer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PathTracer.Samplers;
+
+namespace PathTracer
+{
+    class PathTracer
+    {
+        /// <summary>
+        /// Given Ray r and Scene s, trace the ray over the scene and return the estimated radiance
+        /// </summary>
+        /// <param name="r">Ray direction</param>
+        /// <param name="s">Scene to trace</param>
+        /// <returns>Estimated radiance in the ray direction</returns>
+        public Spectrum Li(Ray r, Scene s)
+        {
+            var L = Spectrum.ZeroSpectrum;
+            /* Implement */
+            return L;
+        }
+
+    }
+}
