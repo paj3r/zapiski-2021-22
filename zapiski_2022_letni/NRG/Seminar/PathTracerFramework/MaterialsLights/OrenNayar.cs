62e5fad983438b4f3c4c83de4487e476b724c47a
paj3r <ivo.pajer@gmail.com>
2022-05-10 11:25:29 +0200

    init seminar

---
+++ zapiski_2022_letni/NRG/Seminar/PathTracerFramework/MaterialsLights/OrenNayar.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathTracer
+{
+    /// <summary>s
+    /// Example BxDF implementation of a perfect lambertian surface
+    /// </summary>
+    public class OrenNayar : BxDF
+    {
+        private Spectrum kd;
+        private double delta;
+        private double albedo;
+        public OrenNayar(Spectrum r, double del)
+        {
+            kd = r;
+            delta = del;
+        }
+
+        /// <summary>
+        /// Lambertian f is kd/pi
+        /// </summary>
+        /// <param name="wo">output vector</param>
+        /// <param name="wi">input vector</param>
+        /// <returns></returns>
+        public override Spectrum f(Vector3 wo, Vector3 wi)
+        {
+            var A = 1 - ((delta * delta) / (2 * ((delta * delta) + 0.33)));
+            var B = 0.45 * ((delta * delta) / ((delta * delta) + 0.09));
+
+            var alpha = new Vector3(0, 0, 0);
+            var beta = new Vector3(0, 0, 0);
+            if (new Vector3(wo.x, wo.y, 0).Length() > (new Vector3(wi.x, wi.y, 0).Length()))
+            {
+                alpha = wo.Clone();
+                beta = wi.Clone();
+            }
+            else
+            {
+                alpha = wi.Clone();
+                beta = wo.Clone();
+            }
+            if (!Utils.SameHemisphere(wo, wi))
+                return Spectrum.ZeroSpectrum;
+            return (kd * Utils.PiInv) * (A + B * Math.Max(0, Utils.CosPhi(wi - wo))
+                * Utils.SinTheta(alpha) * Utils.TanTheta(beta));
+        }
+
+        /// <summary>
+        /// Cosine weighted sampling of wi
+        /// </summary>
+        /// <param name="wo">wo in local</param>
+        /// <returns>(f, wi, pdf)</returns>
+        public override (Spectrum, Vector3, double) Sample_f(Vector3 wo)
+        {
+            var wi = Samplers.CosineSampleHemisphere();
+            if (wo.z < 0)
+                wi.z *= -1;
+            double pdf = Pdf(wo, wi);
+            return (f(wo, wi), wi, pdf);
+        }
+
+        /// <summary>
+        /// returns pdf(wo,wi) as |cosTheta|/pi
+        /// </summary>
+        /// <param name="wo">output vector in local</param>
+        /// <param name="wi">input vector in local</param>
+        /// <returns></returns>
+        public override double Pdf(Vector3 wo, Vector3 wi)
+        {
+            if (!Utils.SameHemisphere(wo, wi))
+                return 0;
+
+            return Math.Abs(wi.z) * Utils.PiInv; // wi.z == cosTheta
+        }
+    }
+}
---
+++ zapiski_2022_letni/NRG/Seminar/PathTracerFramework/Primitives/Sphere.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using MathNet.Numerics.Integration;
+
+namespace PathTracer
+{
+    /// <summary>
+    /// Sphere Shape template class - NOT implemented completely
+    /// </summary>
+    class Sphere : Shape
+    {
+        public double Radius { get; set; }
+        public Sphere(double radius, Transform objectToWorld)
+        {
+            Radius = radius;
+            ObjectToWorld = objectToWorld;
+        }
+
+        /// <summary>
+        /// Ray-Sphere intersection - NOT implemented
+        /// </summary>
+        /// <param name="r">Ray</param>
+        /// <returns>t or null if no hit, point on surface</returns>
+        public override (double?, SurfaceInteraction) Intersect(Ray ray)
+        {
+            //SOURCE: https://github.com/mmp/pbrt-v3/blob/master/src/shapes/sphere.cpp
+            Ray r = WorldToObject.Apply(ray);
+
+            // TODO: Compute quadratic sphere coefficients
+
+            // TODO: Initialize _double_ ray coordinate values
+            double a = r.d.x * r.d.x + r.d.y * r.d.y + r.d.z * r.d.z;
+            double b = 2 * (r.d.x * r.o.x + r.d.y * r.o.y + r.d.z * r.o.z);
+            double c = r.o.x * r.o.x + r.o.y * r.o.y + r.o.z * r.o.z - Radius * Radius;
+
+            // TODO: Solve quadratic equation for _t_ values
+            double t0, t1;
+            bool succ;
+            (succ, t0, t1) = Utils.Quadratic(a, b, c);
+            if (!succ)
+                return (null, null);
+
+            // TODO: Check quadric shape _t0_ and _t1_ for nearest intersection
+            if (t1<= Renderer.Epsilon) return (null,null);
+
+            double tShapeHit = t0;
+            if (tShapeHit <= Renderer.Epsilon)
+            {
+                tShapeHit = t1;
+            }
+            // TODO: Compute sphere hit position and $\phi$
+            var pHit = r.Point(tShapeHit); 
+            var dpdu = new Vector3(-pHit.y, pHit.x, 0);
+            var si = new SurfaceInteraction(pHit, pHit, -r.d, dpdu, this);
+
+            // TODO: Return shape hit and surface interaction
+            return (tShapeHit, ObjectToWorld.Apply(si));
+        }
+
+        /// <summary>
+        /// Sample point on sphere in world
+        /// </summary>
+        /// <returns>point in world, pdf of point</returns>
+        public override (SurfaceInteraction, double) Sample()
+        {
+            // TODO: Implement Sphere sampling
+            Vector3 vec = Samplers.UniformSampleSphere()*Radius;
+            var dpdu = new Vector3(-vec.y, vec.x, 0);
+            var pdf = 1 / Area();
+            var nor = ObjectToWorld.ApplyNormal(vec);
+            // TODO: Return surface interaction and pdf
+            return (ObjectToWorld.Apply(new SurfaceInteraction(vec, nor, 
+                Vector3.ZeroVector, dpdu, this)), pdf);
+        }
+
+        public override double Area() { return 4 * Math.PI * Radius * Radius; }
+
+        /// <summary>
+        /// Estimates pdf of wi starting from point si
+        /// </summary>
+        /// <param name="si">point on surface that wi starts from</param>
+        /// <param name="wi">wi</param>
+        /// <returns>pdf of wi given this shape</returns>
+        public override double Pdf(SurfaceInteraction si, Vector3 wi)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
